14ac334bd22e625dbc3e206d51ac99f0db42bf05
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-09-01 01:23:21 -0300

    utils: add document generators

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 const getLogger = require('./logger')
 const generateText = require('./generateText')
 const formatError = require('./formatError')
+const documents = require('./documents')
 
 module.exports = {
   getLogger,
   generateText,
   formatError,
+  documents,
 }
