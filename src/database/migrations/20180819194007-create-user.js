6b00f4455fe134c21ccd7264a46547f25fb0ef3b
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-09-01 01:20:40 -0300

    database: add user migration

---
+++ src/database/migrations/20180819194007-create-user.js
@@ -0,0 +1,54 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('user', {
+    id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      defaultValue: Sequelize.UUIDV1,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING(200),
+      allowNull: false,
+    },
+    documentId: {
+      type: Sequelize.STRING(20),
+      allowNull: false,
+    },
+    documentType: {
+      type: Sequelize.ENUM('cpf', 'rg'),
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    companyId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'company',
+        key: 'id',
+      },
+      allowNull: false,
+      onUpdate: 'cascade',
+      onDelete: 'NO ACTION',
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('user'),
+}
