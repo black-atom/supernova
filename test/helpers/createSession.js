4cebc39b6ee1868b8062e87bbfb59f4be4973a74
alexandrejuk <ale_santos.soares@hotmail.com>
2018-09-10 16:22:43 -0300

    tests: fix request timeout

--- test/helpers/createSession.js
+++ test/helpers/createSession.js
@@ -2,10 +2,12 @@ import getRequest from './getRequest'
 
 const request = getRequest()
 
-const createSession = (username, password) => request
+const createSession = (username, password, type = 'browser') => request
   .post('/login', {
     username,
     password,
+    type,
   })
+  .then(response => response.body)
 
 export default createSession
--- test/helpers/getTemporaryCompany.js
+++ test/helpers/getTemporaryCompany.js
@@ -4,5 +4,6 @@ const request = getRequest()
 
 const getTemporaryCompany = () => request
   .post('/temporary')
+  .then(response => response.body)
 
 export default getTemporaryCompany
