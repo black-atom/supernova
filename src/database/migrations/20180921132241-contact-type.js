1ff3ffcde1948b54a754f2f49f84c3444c116898
alexandrejuk <ale_santos.soares@hotmail.com>
2018-09-21 10:32:38 -0300

    database: add address_type and contact_type migration

---
+++ src/database/migrations/20180921132241-contact-type.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface
+    .createTable('contact_type', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV1,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(200),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+  down: (queryInterface) => {
+    queryInterface.dropTable('contact_type')
+  },
+}
