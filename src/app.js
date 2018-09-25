46782729baae285da6fec6aa58db02cdb8b5b68c
alexandrejuk <ale_santos.soares@hotmail.com>
2018-09-25 16:32:42 -0300

    chore: disabled on line eslint

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res, next) => { //eslint-disable-line
 
+  /* eslint-disable no-console */
   console.error(err.stack || err)
   const formattedError = formatError(err)
 
